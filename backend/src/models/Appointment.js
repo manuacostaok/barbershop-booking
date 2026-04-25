cdcf036b611204919c0c0f149b06842361f101c4
Manu Acosta <manuacostaok@gmail.com>
2026-04-24 23:03:15 -0300

    feat: add Appointment model

---
+++ backend/src/models/Appointment.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const appointmentSchema = new mongoose.Schema(
+  {
+    clientName: {
+      type: String,
+      required: true,
+    },
+    service: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String, // ej: "2026-04-25"
+      required: true,
+    },
+    time: {
+      type: String, // ej: "14:30"
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "cancelled"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Appointment", appointmentSchema);
