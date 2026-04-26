1b1ca37ada466adf5e6d05429af514ae09cbbe84
Manu Acosta <manuacostaok@gmail.com>
2026-04-26 12:02:35 -0300

    feat: filtro por fecha con dropdawn

--- frontend/src/pages/AdminPanel.jsx
+++ frontend/src/pages/AdminPanel.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { motion } from "framer-motion";
 import ConfirmModal from "../components/ConfirmModal";
+import Calendar from "react-calendar";
 
 function AdminPanel() {
   const [appointments, setAppointments] = useState([]);
@@ -9,6 +10,12 @@ function AdminPanel() {
 
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedId, setSelectedId] = useState(null);
+
+  const [filterDate, setFilterDate] = useState("");
+  const [filterBarber, setFilterBarber] = useState("");
+  const [barbers, setBarbers] = useState([]);
+
+  const [showCalendar, setShowCalendar] = useState(false);
 
   const openModal = (id) => {
     setSelectedId(id);
@@ -47,6 +54,34 @@ function AdminPanel() {
     fetchAppointments();
   }, []);
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/users/barbers")
+      .then((res) => setBarbers(res.data))
+      .catch(() => console.log("Error cargando barberos"));
+  }, []);
+
+
+  const formatDate = (date) => {
+    if (!date) return "";
+
+    return (
+      date.getFullYear() +
+      "-" +
+      String(date.getMonth() + 1).padStart(2, "0") +
+      "-" +
+      String(date.getDate()).padStart(2, "0")
+    );
+  };
+
+  const filteredAppointments = appointments.filter((appt) => {
+    return (
+      (!filterDate || appt.date === formatDate(filterDate)) &&
+      (!filterBarber || appt.barber?._id === filterBarber)
+    );
+  });
+
+
   return (
     <div className="container">
       <motion.div
@@ -56,13 +91,51 @@ function AdminPanel() {
       >
         <div className="title">Panel Admin</div>
 
+        
+        <div className="filters">
+          {/* BOTÓN */}
+          <button
+            className="button"
+            onClick={() => setShowCalendar(!showCalendar)}
+          >
+            {filterDate
+              ? `Fecha: ${formatDate(filterDate)}`
+              : "Elegir fecha"}
+          </button>
+
+          {/* SELECT BARBERO */}
+          <select
+            className="input"
+            value={filterBarber}
+            onChange={(e) => setFilterBarber(e.target.value)}
+          >
+            <option value="">Todos los barberos</option>
+            {barbers.map((b) => (
+              <option key={b._id} value={b._id}>
+                {b.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        {showCalendar && (
+          <div className="calendar-dropdown">
+            <Calendar
+              onChange={(date) => {
+                setFilterDate(date);
+                setShowCalendar(false); // 🔥 se cierra solo
+              }}
+              value={filterDate}
+            />
+          </div>
+        )}
+
         {loading ? (
           <p>Cargando...</p>
         ) : appointments.length === 0 ? (
           <p>No hay turnos</p>
         ) : (
           <div className="admin-list">
-            {appointments.map((appt) => (
+            {filteredAppointments.map((appt) => (
               <div key={appt._id} className="row">
                 <div>{appt.clientName}</div>
                 <div>{appt.service}</div>
