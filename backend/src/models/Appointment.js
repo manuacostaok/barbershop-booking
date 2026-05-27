98e0cf3022618abb886767095a9113da6d723d5c
Manu Acosta <manuacostaok@gmail.com>
2026-05-26 21:20:13 -0300

    final fix de la feat turnos completos

--- backend/src/models/Appointment.js
+++ backend/src/models/Appointment.js
@@ -53,7 +53,7 @@ const appointmentSchema = new mongoose.Schema(
 
     status: {
       type: String,
-      enum: ["pending", "confirmed", "cancelled"],
+      enum: ["pending", "confirmed", "cancelled", "completed"],
       default: "confirmed",
     },
   },
